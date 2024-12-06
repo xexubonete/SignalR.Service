18be90e2f3599b5bb339aa80685cf72f314ad9b7
xexubonete <xexubonete@gmail.com>
2024-12-07 00:28:32 +0100

    Resolving problems w dependencies

--- Hubs.cs
+++ Hubs.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
-namespace SignalR.Service.Hubs;
-
-public class ChatHub : Hub
+namespace SignalR.Service.Hubs
 {
-    public async Task SendMessage(string user, string message)
+    public class ChatHub : Hub
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        private static readonly ConcurrentDictionary<string, string> connections = new ConcurrentDictionary<string, string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            string connectionId = Context.ConnectionId;
+            string clientIp = Context.GetHttpContext()?.Connection.RemoteIpAddress?.ToString();
+
+            if (!string.IsNullOrEmpty(clientIp))
+            {
+                connections[connectionId] = clientIp;
+                await Clients.All.SendAsync("notify", $"Cliente conectado: {connectionId} desde IP {clientIp}");
+            }
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            string connectionId = Context.ConnectionId;
+
+            if (connections.TryRemove(connectionId, out string clientIp))
+            {
+                await Clients.All.SendAsync("notify", $"Cliente desconectado: {connectionId} que estaba en la IP {clientIp}");
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
-
